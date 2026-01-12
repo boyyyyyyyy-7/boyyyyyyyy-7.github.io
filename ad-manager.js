157ebdc7775851cacd6c3719ffce25865c54c783
Braydenistallgames <BraydenBush@proton.me>
2026-01-11 23:21:50 -0500

    Add files via upload

--- ad-manager.js
+++ ad-manager.js
@@ -8,19 +8,3 @@
  * - Protocol-aware loading (file:// check)
  */
 
-(function () {
-    const activeScriptIds = ['nap5k-main-script', 'ezoic-script', 'gk-1', 'gk-2'];
-
-    function clearAdSystems() {
-        activeScriptIds.forEach(id => {
-            const el = document.getElementById(id);
-            if (el) el.remove();
-        });
-
-        document.querySelectorAll('script[src*="ezojs"], script[src*="gatekeeper"], script[src*="nap5k"], script[src*="googlesyndication"], script[src*="doubleclick"]').forEach(s => s.remove());
-        document.querySelectorAll('iframe[src*="doubleclick"], iframe[src*="googlesyndication"], iframe[src*="nap5k"], iframe[src*="ezojs"], iframe[src*="gatekeeper"]').forEach(f => f.remove());
-        document.querySelectorAll('ins.adsbygoogle').forEach(i => i.remove());
-    }
-
-    clearAdSystems();
-})();
